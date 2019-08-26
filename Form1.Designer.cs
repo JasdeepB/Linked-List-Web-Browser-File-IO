1d5d1a0ce65f7e14d80e9c60b62d487db754fc05
Jasdeep Brar <jasdeepbrar187@gmail.com>
2019-08-25 22:09:51 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Grapple
+{
+    partial class Grapple
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Grapple));
+            this.findVideoButton = new System.Windows.Forms.Button();
+            this.nowPlayingTextBox = new System.Windows.Forms.TextBox();
+            this.viewHistory = new System.Windows.Forms.Button();
+            this.browserHistoryBox = new System.Windows.Forms.TextBox();
+            this.addToLibrary = new System.Windows.Forms.Button();
+            this.currentStateBanner = new System.Windows.Forms.RichTextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.hideHistoryButton = new System.Windows.Forms.Button();
+            this.newFileBtn = new System.Windows.Forms.Button();
+            this.userMessage = new System.Windows.Forms.RichTextBox();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.curPathBanner = new System.Windows.Forms.RichTextBox();
+            this.clearPathBtn = new System.Windows.Forms.Button();
+            this.clearHistoryBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // findVideoButton
+            // 
+            this.findVideoButton.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.findVideoButton, "findVideoButton");
+            this.findVideoButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.findVideoButton.Name = "findVideoButton";
+            this.findVideoButton.UseVisualStyleBackColor = false;
+            this.findVideoButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // nowPlayingTextBox
+            // 
+            this.nowPlayingTextBox.BackColor = System.Drawing.Color.DarkSlateGray;
+            resources.ApplyResources(this.nowPlayingTextBox, "nowPlayingTextBox");
+            this.nowPlayingTextBox.ForeColor = System.Drawing.Color.White;
+            this.nowPlayingTextBox.Name = "nowPlayingTextBox";
+            this.nowPlayingTextBox.TextChanged += new System.EventHandler(this.NowPlayingTextBox_TextChanged);
+            // 
+            // viewHistory
+            // 
+            this.viewHistory.BackColor = System.Drawing.Color.DeepSkyBlue;
+            resources.ApplyResources(this.viewHistory, "viewHistory");
+            this.viewHistory.Name = "viewHistory";
+            this.viewHistory.UseVisualStyleBackColor = false;
+            this.viewHistory.Click += new System.EventHandler(this.ViewHistory_Click);
+            // 
+            // browserHistoryBox
+            // 
+            this.browserHistoryBox.AcceptsReturn = true;
+            this.browserHistoryBox.AcceptsTab = true;
+            this.browserHistoryBox.AllowDrop = true;
+            this.browserHistoryBox.BackColor = System.Drawing.SystemColors.Info;
+            resources.ApplyResources(this.browserHistoryBox, "browserHistoryBox");
+            this.browserHistoryBox.Name = "browserHistoryBox";
+            this.browserHistoryBox.ReadOnly = true;
+            this.browserHistoryBox.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // addToLibrary
+            // 
+            this.addToLibrary.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.addToLibrary, "addToLibrary");
+            this.addToLibrary.ForeColor = System.Drawing.Color.White;
+            this.addToLibrary.Name = "addToLibrary";
+            this.addToLibrary.UseVisualStyleBackColor = false;
+            this.addToLibrary.Click += new System.EventHandler(this.AddToLibrary_Click);
+            // 
+            // currentStateBanner
+            // 
+            this.currentStateBanner.BackColor = System.Drawing.Color.BlueViolet;
+            resources.ApplyResources(this.currentStateBanner, "currentStateBanner");
+            this.currentStateBanner.ForeColor = System.Drawing.Color.White;
+            this.currentStateBanner.Name = "currentStateBanner";
+            this.currentStateBanner.TextChanged += new System.EventHandler(this.NowPlayingBanner_TextChanged);
+            // 
+            // progressBar
+            // 
+            resources.ApplyResources(this.progressBar, "progressBar");
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Click += new System.EventHandler(this.ProgressBar1_Click);
+            // 
+            // hideHistoryButton
+            // 
+            this.hideHistoryButton.AutoEllipsis = true;
+            resources.ApplyResources(this.hideHistoryButton, "hideHistoryButton");
+            this.hideHistoryButton.ForeColor = System.Drawing.Color.Black;
+            this.hideHistoryButton.Name = "hideHistoryButton";
+            this.hideHistoryButton.UseVisualStyleBackColor = true;
+            this.hideHistoryButton.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // newFileBtn
+            // 
+            resources.ApplyResources(this.newFileBtn, "newFileBtn");
+            this.newFileBtn.ForeColor = System.Drawing.Color.Black;
+            this.newFileBtn.Name = "newFileBtn";
+            this.newFileBtn.UseVisualStyleBackColor = true;
+            this.newFileBtn.Click += new System.EventHandler(this.NewFileBtn_Click);
+            // 
+            // userMessage
+            // 
+            this.userMessage.BackColor = System.Drawing.Color.GreenYellow;
+            resources.ApplyResources(this.userMessage, "userMessage");
+            this.userMessage.Name = "userMessage";
+            // 
+            // helpButton
+            // 
+            this.helpButton.BackColor = System.Drawing.Color.LightYellow;
+            resources.ApplyResources(this.helpButton, "helpButton");
+            this.helpButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.helpButton.Name = "helpButton";
+            this.helpButton.UseVisualStyleBackColor = false;
+            this.helpButton.Click += new System.EventHandler(this.Button1_Click_2);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.filePathTextBox, "filePathTextBox");
+            this.filePathTextBox.ForeColor = System.Drawing.Color.GhostWhite;
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.TextChanged += new System.EventHandler(this.FilePathTextBox_TextChanged);
+            // 
+            // curPathBanner
+            // 
+            this.curPathBanner.BackColor = System.Drawing.Color.DodgerBlue;
+            resources.ApplyResources(this.curPathBanner, "curPathBanner");
+            this.curPathBanner.Name = "curPathBanner";
+            // 
+            // clearPathBtn
+            // 
+            resources.ApplyResources(this.clearPathBtn, "clearPathBtn");
+            this.clearPathBtn.ForeColor = System.Drawing.Color.Black;
+            this.clearPathBtn.Name = "clearPathBtn";
+            this.clearPathBtn.UseVisualStyleBackColor = true;
+            this.clearPathBtn.Click += new System.EventHandler(this.ClearPathBtn_Click);
+            // 
+            // clearHistoryBtn
+            // 
+            resources.ApplyResources(this.clearHistoryBtn, "clearHistoryBtn");
+            this.clearHistoryBtn.ForeColor = System.Drawing.Color.Black;
+            this.clearHistoryBtn.Name = "clearHistoryBtn";
+            this.clearHistoryBtn.UseVisualStyleBackColor = true;
+            this.clearHistoryBtn.Click += new System.EventHandler(this.ClearHistoryBtn_Click);
+            // 
+            // Grapple
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.clearHistoryBtn);
+            this.Controls.Add(this.clearPathBtn);
+            this.Controls.Add(this.curPathBanner);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.userMessage);
+            this.Controls.Add(this.newFileBtn);
+            this.Controls.Add(this.hideHistoryButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.currentStateBanner);
+            this.Controls.Add(this.addToLibrary);
+            this.Controls.Add(this.browserHistoryBox);
+            this.Controls.Add(this.viewHistory);
+            this.Controls.Add(this.nowPlayingTextBox);
+            this.Controls.Add(this.findVideoButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Grapple";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button findVideoButton;
+        private System.Windows.Forms.TextBox nowPlayingTextBox;
+        private System.Windows.Forms.Button viewHistory;
+        private System.Windows.Forms.TextBox browserHistoryBox;
+        private System.Windows.Forms.Button addToLibrary;
+        private System.Windows.Forms.RichTextBox currentStateBanner;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button hideHistoryButton;
+        private System.Windows.Forms.Button newFileBtn;
+        private System.Windows.Forms.RichTextBox userMessage;
+        private System.Windows.Forms.Button helpButton;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.RichTextBox curPathBanner;
+        private System.Windows.Forms.Button clearPathBtn;
+        private System.Windows.Forms.Button clearHistoryBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Grapple
+{
+    public partial class Grapple : Form
+    {
+        public string filePath = @"";
+
+        private VideoHistory top = null;
+
+        public Grapple()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (progressBar.Value != 0)
+            {
+                progressBar.Value = 0;
+            }
+
+            if (filePath != "")
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    progressBar.Increment(1);
+                }
+
+                currentStateBanner.Visible = true;
+                currentStateBanner.Text = "       Now Visiting";
+
+                FindPath();
+            }
+            else
+            {
+                string message = "\tNo file is currently selected! \n Click 'Change File' then select the target file";
+                string title = "Reference file not found!";
+
+                MessageBox.Show(message, title);
+            }
+        }
+
+        public void FindPath()
+        {
+            try
+            {
+                char[] delimimeters = { '\n' };
+                int itemsOnList = 0;
+
+
+                using (TextReader file = new StreamReader(filePath))
+                {
+
+                    string sLine = file.ReadLine();
+
+                    while (sLine != null)
+                    {
+                        string[] tokensFromLine = sLine.Split(delimimeters, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (string token in tokensFromLine)
+                        {
+                            //Console.WriteLine("{0}", tokensFromLine[0]);
+                            itemsOnList++;
+                            break;
+                        }
+
+                        sLine = file.ReadLine();
+                    }
+
+                }
+
+                FindVideo(itemsOnList);
+            }
+            catch(System.ArgumentException)
+            {
+                string message = "\tNo file is currently selected! \n Click 'Change File' then select the target file";
+                string title = "Reference file not found!";
+
+                MessageBox.Show(message, title);
+            }
+        }
+
+        public void FindVideo(int librarySize)
+        {
+            try
+            {
+                Random generateThis = new Random();
+                char[] delimimeters = { '\n' };
+                string[] video = new string[librarySize];
+                int index = 0;
+
+                using (TextReader theFile = new StreamReader(filePath))
+                {
+                    string sLine = theFile.ReadLine();
+
+                    while (sLine != null)
+                    {
+                        string[] tokensFromLine = sLine.Split(delimimeters, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (string token in tokensFromLine)
+                        {
+                            video[index] = tokensFromLine[0];
+                            index++;
+                        }
+
+                        sLine = theFile.ReadLine();
+                    }
+                    int videoFound = generateThis.Next(0, librarySize);
+                    nowPlayingTextBox.Text = video[videoFound];
+                    AddToHistory(video[videoFound]);
+
+                    using (var process = new Process())
+                    {
+                        process.StartInfo.FileName = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+                        process.StartInfo.Arguments = video[videoFound] + " --incognito";
+
+                        process.Start();
+                    }
+
+                    //**DEBUG**
+                    //Console.WriteLine("{0}", video[videoFound]);
+                    //Console.WriteLine("Loop {0}", index);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                string message = "A wild IndexOutOfRangeException has been spotted.\n\nTry again?";
+                string title = "Error!";
+
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show(message, title, buttons);
+
+                if (result == DialogResult.Yes)
+                {
+                    FindVideo(librarySize);
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            
+        }
+        private class VideoHistory
+        {
+            public string data;
+            public VideoHistory next;
+
+            public VideoHistory(string x)
+            {
+                data = x;
+            }
+        }
+
+        public void AddToHistory(string videoToAdd)
+        {
+            if (top == null)
+            {
+                VideoHistory newVideo = new VideoHistory(videoToAdd);
+                top = newVideo;
+            }
+            else
+            {
+                VideoHistory newVideo = new VideoHistory(videoToAdd);
+
+                newVideo.next = top;
+                top = newVideo;
+            }
+        }
+
+        public void PrintHistory()
+        {
+            VideoHistory cur = top;
+            string currentHistory = "";
+            int videoNumber = 1;
+
+            while (cur != null)
+            {
+                currentHistory += videoNumber + ": " + cur.data + Environment.NewLine;
+                videoNumber++;
+                cur = cur.next;
+            }
+
+            browserHistoryBox.Text = currentHistory;
+        }
+
+
+        private void NowPlayingTextBox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ViewHistory_Click(object sender, EventArgs e)
+        {
+            if (top == null)
+            {
+                return;
+            }
+            else
+            {
+                browserHistoryBox.Visible = true;
+                hideHistoryButton.Visible = true;
+                currentStateBanner.Visible = true;
+                clearHistoryBtn.Visible = true;
+                currentStateBanner.Text = "     Viewing History";
+                PrintHistory();
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void AddToLibrary_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                currentStateBanner.Visible = true;
+                currentStateBanner.Text = "     Editing Library";
+                Process.Start("explorer.exe", "/select," + filePath);
+            }
+        }
+
+        private void NowPlayingBanner_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void HistoryBanner_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            if (browserHistoryBox.Visible == true && hideHistoryButton.Visible == true)
+            {
+                browserHistoryBox.Visible = false;
+                hideHistoryButton.Visible = false;
+                clearHistoryBtn.Visible = false;
+                currentStateBanner.Text = "           Grapple";
+            }
+        }
+
+        public void SelectFile()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text file (*.txt)|*.txt";
+            openFile.FilterIndex = 2;
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFile.FileName;
+            }
+        }
+
+        private void NewFileBtn_Click(object sender, EventArgs e)
+        {
+            currentStateBanner.Visible = true;
+            currentStateBanner.Text = "   Selecting Source";
+            SelectFile();
+            filePathTextBox.Text = filePath;
+            
+            if (filePath == "")
+            {
+                clearPathBtn.Visible = false;
+            }
+            else
+            {
+                clearPathBtn.Visible = true;
+            }
+        }
+
+        private void Button1_Click_2(object sender, EventArgs e)
+        {
+            currentStateBanner.Visible = true;
+            currentStateBanner.Text = "Viewing Demo File";
+            Process.Start("notepad.exe", "C:\\Program Files (x86)\\Grapple Tech\\Grapple\\Example File.txt");
+        }
+
+        private void ClearPathBtn_Click(object sender, EventArgs e)
+        {
+            if (filePath != "")
+            {
+                currentStateBanner.Text = "Cleared file reference";
+
+                filePath = "";
+                filePathTextBox.Text = filePath;
+                clearPathBtn.Visible = false;
+            }
+        }
+
+        private void ClearHistoryBtn_Click(object sender, EventArgs e)
+        {
+            if (top != null)
+            {
+                top = null;
+                PrintHistory();
+            }
+        }
+    }
+}
